df28d3a4593ed7239d46c885b0abb41ef14b7b72
“Brian <brianbolnick@gmail.com>
2018-05-19 10:18:59 -0600

    route fix

--- client/src/tools/api-config.js
+++ client/src/tools/api-config.js
@@ -6,7 +6,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:5000';
 } else {
-  backendHost = 'https://our-europe-trip.herokuapp.com/';
+  backendHost = 'https://our-europe-trip.herokuapp.com';
 }
 
 export const API_URL = `${backendHost}`;
